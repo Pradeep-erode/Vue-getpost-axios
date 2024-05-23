aaf6e43b18697d2bd2e76ada1057b7903298bd5b
Pradeep <pradeepcpk1997@gmail.com>
2024-05-23 21:34:57 +0530

    first commit

---
+++ src/components/Getmethod.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h5 v-if="errorMsg">{{errorMsg}}</h5>
+    <button @click="getmethod">Load posts</button>
+  <div v-for="post in posts" :key="post.id">
+<h4> {{post.id}}. {{post.title}}</h4>  
+    <p>{{post.body}}</p>
+    <hr/>
+</div>
+  </div>
+</template>
+
+<script>
+
+
+import axios from 'axios'
+
+export default {
+name:'api-get',
+data(){
+    return{
+        posts:[],
+        errorMsg:''
+    }
+},
+methods:{
+    getmethod(){
+        axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then((response)=>{
+            this.posts=response.data
+        }).catch((error)=>{
+            this.errorMsg=error
+        })
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Postmethod.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- we use prevent keyword to prevent refresh of the data given -->
+  <form @submit.prevent="Post">
+    <div>
+        <label for="userId">UserId</label>
+    <input type="text" id="userId" >
+    </div>
+    <div>
+        <label for="title">Title</label>
+    <input type="text" id="title" >
+    </div>
+    <div>
+        <label for="body">Body message</label>
+    <input type="text" id="body" >
+    </div>
+    <button>PostData</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name:'api-post',
+    data(){
+        return{
+            formData:{
+                userId:'',
+                title:'',
+                body:''
+            }
+        }
+    },
+    methods:{
+        Post(){
+            axios.post('https://jsonplaceholder.typicode.com/posts',this.formData)
+            .then((response)=>{
+console.log(response)
+            }).catch((error)=>{
+                console.log(error)
+            })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
